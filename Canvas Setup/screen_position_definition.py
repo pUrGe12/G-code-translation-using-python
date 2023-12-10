72156e2c528a66ecfef29cdeed9ed8e86e23070a
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2023-12-10 20:15:29 +0530

    Update screen_position_definition.py

--- Canvas Setup/screen_position_definition.py
+++ Canvas Setup/screen_position_definition.py
@@ -2,7 +2,7 @@ import turtle
 
 def turtle_set_up():
     '''
-    I've defined a canvas (1024,600), to avoid bind and terminator issues the RUNNING parameter is manually set to true.
+    I've defined a canvas (1024,600), to avoid bind and terminator issues the RUNNING parameter is manually set to True.
     
     '''
     
