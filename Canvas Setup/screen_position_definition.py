f96c0c9c60c634c9c4d8e966409c0913c2441a9a
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2023-12-11 14:06:44 +0530

    Update screen_position_definition.py

--- Canvas Setup/screen_position_definition.py
+++ Canvas Setup/screen_position_definition.py
@@ -2,13 +2,13 @@ import turtle
 
 def turtle_set_up():
     '''
-    I've defined a canvas (1024,600), to avoid bind and terminator issues the RUNNING parameter is manually set to True.
+    I've defined a canvas (2048,600), to avoid bind and terminator issues the RUNNING parameter is manually set to True.
     
     '''
     
     screen = turtle.Screen()
     screen.title('G-code visualisation')
-    screen.setup(width = 1024, height = 600) # width is the x-axis and height is the y-axis
+    screen.setup(width = 2048, height = 600) # width is the x-axis and height is the y-axis
     
     turtle.TurtleScreen._RUNNING = True
     turtle.speed(0)
@@ -16,7 +16,7 @@ def turtle_set_up():
     
     # start positioning
     turtle.penup()
-    turtle.goto(-400, -150)
+    turtle.goto(-1024, -150)
     turtle.left(90)
     
     screen.exitonclick()
