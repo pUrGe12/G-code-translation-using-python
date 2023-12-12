fed247e4dfb1724cdf6b13e32d39023210c12289
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2023-12-12 11:46:04 +0530

    Add files via upload

---
+++ Test/case_2/case_2.py
@@ -0,0 +1,14 @@
+# this code is to scrape the G-code for boltpattern
+ 
+import requests
+from bs4 import BeautifulSoup
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+cookie = '_zitok=50eb0870dd8a91c1a8411702360585'
+header = {'User-Agent': user_agent, 'Cookie': cookie}
+
+web = requests.get('https://machmotion.com/downloads/GCode/boltpattern.txt', headers = header).text
+soup = BeautifulSoup(web, 'lxml')
+
+G_code_unlisted = soup.find('p').text
+G_code_listed = soup.find('p').text.split()
